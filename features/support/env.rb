1611ea2fde4e78579691e37a7a851ed104643f01
Sam Jones <sam@samljones.com>
2015-10-09 08:33:23 -0400

    updating to use firefox driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,14 +1,9 @@
 require 'capybara'
 include Capybara::DSL
 
-Capybara.register_driver :chrome do |app|
-	  Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
-
-
 Capybara.configure do |config|
 	config.run_server = false
-	config.default_driver = :chrome
+	config.default_driver = :selenium
 	config.app_host = 'https://localhost:8080'
 end
 
